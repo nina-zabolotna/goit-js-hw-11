62aecc26cbd2a476d0cb429cea481edf5bbac0fc
nina-zabolotna <116902896+nina-zabolotna@users.noreply.github.com>
2023-10-29 14:48:14 +0000

    Deploying to gh-pages from @ nina-zabolotna/goit-js-hw-11@e9f8f37b8435616c08f9c21e55e6745365b2b743 🚀

---
+++ index.0b7d4c30.js
@@ -0,0 +1,2 @@
+!function(){var o=$("#button");$(window).scroll((function(){$(window).scrollTop()>300?o.addClass("show"):o.removeClass("show")})),o.on("click",(function(o){o.preventDefault(),$("html, body").animate({scrollTop:0},"300")}))}();
+//# sourceMappingURL=index.0b7d4c30.js.map
